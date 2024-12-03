5b69fd32ca4b59f1a1a2bb239ab30a27ddbf2f74
Joona Halonen <joona.halonen@cgi.com>
2024-12-03 16:46:06 +0200

    Fix ouput if dev-portal is not installed.

--- ewc/outuputs.tf
+++ ewc/outuputs.tf
@@ -28,7 +28,7 @@ output "vault_pod_ready_statuses_after_init" {
 
 output "dev-portal_keycloak_secret" {
   description = "Dev-portal's secret to authenticate with Keycloak"
-  value       = module.dev-portal-init.0.dev-portal_keycloak_secret
+  value       = try(module.dev-portal-init.0.dev-portal_keycloak_secret, null)
   sensitive   = true
 }
 
