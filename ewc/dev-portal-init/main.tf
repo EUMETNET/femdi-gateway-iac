4f4e0e2e5b4accd831104a9ba6af6ec3deb39d5c
Joonas Koskela <joonas.koskela@fmi.fi>
2026-04-01 14:21:51 +0300

    Fix correct dev-portal images
    The commit af7192c96eba1eee64a086ca91d20d3161b9ee2e
    had the correct image tag values but the fronted had the backend
    and the backend had the frontend image values.
    
    https://jira.fmi.fi/browse/STU-30365

--- ewc/dev-portal-init/main.tf
+++ ewc/dev-portal-init/main.tf
@@ -276,7 +276,7 @@ resource "helm_release" "dev-portal" {
     },
     {
       name  = "backend.image.tag"
-      value = "sha-63fa1a4"
+      value = "sha-7693b67"
     },
     {
       name  = "backend.secrets.secretName"
@@ -284,7 +284,7 @@ resource "helm_release" "dev-portal" {
     },
     {
       name  = "frontend.image.tag"
-      value = "sha-7693b67"
+      value = "sha-63fa1a4"
     },
     {
       name  = "frontend.keycloak_logout_url"
